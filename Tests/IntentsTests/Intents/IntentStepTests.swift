93b3d9deda21faea76daecfab4dad4539c556a2d
adamz <adam@zethrae.us>
2023-06-08 22:59:57 -0700

    Add IntentStepTests

--- Tests/IntentsTests/Intents/IntentStepTests.swift
+++ Tests/IntentsTests/Intents/IntentStepTests.swift
@@ -4,59 +4,49 @@ import XCTest
 // MARK: - IntentStepTests
 
 final class IntentStepTests: XCTestCase {
-//
-//  func test_intentStep_decoding() throws {
-//    let expected = FlatIntent(payload: "somepayload")
-//    let step = try Step(name: "myintent", fields: ["payload": "somepayload"])
-//    let decoded = try step.decode(as: FlatIntent.self)
-//    XCTAssertEqual(expected, decoded)
-//  }
-//
-//  func test_nestingIntentStep_decoding() throws {
-//    let expected = NestedIntent(
-//      subStruct: .init(
-//        field1: true,
-//        field2: 22,
-//        field3: "333"
-//      ),
-//      loneField: 44.44,
-//      subEnum: .one
-//    )
-//    let step = try Step(
-//      name: "typenesting",
-//      fields: [
-//        "subStruct": [
-//          "field1": true,
-//          "field2": 22,
-//          "field3": "333",
-//        ] as [String: Any],
-//        "loneField": 44.44,
-//        "subEnum": "one",
-//      ]
-//    )
-//    let decoded = try step.decode(as: NestedIntent.self)
-//    XCTAssertEqual(expected, decoded)
-//  }
-//
-//  func test_intentStep_decoding_failsByName() throws {
-//    let step = try Step(name: "otherintent", fields: ["payload": "somepayload"])
-//    XCTAssertThrowsError(
-//      try step.decode(as: FlatIntent.self)
-//    )
-//  }
-//
-//  func test_intentStep_decoding_failsByFields() throws {
-//    let step = try Step(name: "myintent", fields: ["playado": "somepayload"])
-//    XCTAssertThrowsError(
-//      try step.decode(as: FlatIntent.self)
-//    )
-//  }
+
+  func test_nestingIntentStep_decoding() throws {
+    let expected = NestedIntent(
+      subStruct: .init(
+        field1: true,
+        field2: 22,
+        field3: "333"
+      ),
+      loneField: 44.44,
+      subEnum: .one
+    )
+    let step = Step(
+      NestedIntent(
+        subStruct: .init(
+          field1: true,
+          field2: 22,
+          field3: "333"
+        ),
+        loneField: 44.44,
+        subEnum: .one
+      )
+    )
+    let decoded = try step.getPayload(as: NestedIntent.self)
+    XCTAssertEqual(expected, decoded)
+  }
+
+  func test_intentStep_decoding_failsB() throws {
+    let step = Step(SomeIntent(payload: "hi"))
+    XCTAssertThrowsError(
+      try step.getPayload(as: FlatIntent.self)
+    )
+  }
 }
 
 extension IntentStepTests {
 
   struct FlatIntent: StepPayload {
     static let name = "myintent"
+    let payload: String
+  }
+
+  struct SomeIntent: StepPayload {
+    static let name = "otherintent"
     let payload: String
   }
 
